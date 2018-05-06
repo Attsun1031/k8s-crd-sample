af28c366b3450503a1042e2c8e4f721f7e1b97be
Attsun <atsumi.tatsuya@gmail.com>
2018-05-06 14:38:36 +0900

    delete comment

--- pkg/controller.go
+++ pkg/controller.go
@@ -25,15 +25,11 @@ import (
 const controllerAgentName = "jobnetes-controller"
 
 const (
-	// SuccessSynced is used as part of the Event 'reason' when a Workflow is synced
 	SuccessSynced = "Synced"
 
-	// MessageResourceSynced is the message used for an Event fired when a Workflow
-	// is synced successfully
 	MessageResourceSynced = "Workflow synced successfully"
 )
 
-// Controller is the controller implementation for Workflow resources
 type Controller struct {
 	kubeclientset     kubernetes.Interface
 	workflowClientset clientset.Interface
@@ -41,28 +37,17 @@ type Controller struct {
 	workflowLister listers.WorkflowLister
 	workflowSynced cache.InformerSynced
 
-	// workqueue is a rate limited work queue. This is used to queue work to be
-	// processed instead of performing it as soon as a change happens. This
-	// means we can ensure we only process a fixed amount of resources at a
-	// time, and makes it easy to ensure we are never processing the same item
-	// simultaneously in two different workers.
 	workqueue workqueue.RateLimitingInterface
-	// recorder is an event recorder for recording Event resources to the
-	// Kubernetes API.
 	recorder record.EventRecorder
 }
 
-// NewController returns a new sample controller
 func NewController(
 	kubeclientset kubernetes.Interface,
 	workflowClientset clientset.Interface,
 	workflowInformerFactory informers.SharedInformerFactory) *Controller {
 
-	// obtain references to shared index informers for the Workflow types.
 	wfInformer := workflowInformerFactory.Workflow().V1beta().Workflows()
 
-	// Create event broadcaster
-	// Add controller types to the default Kubernetes Scheme so Events can be logged for controller types.
 	workflowscheme.AddToScheme(scheme.Scheme)
 	glog.Info("Creating event broadcaster")
 
@@ -81,7 +66,6 @@ func NewController(
 	}
 
 	glog.Info("Setting up event handlers")
-	// Set up an event handler for when Workflow resources change
 	wfInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: controller.enqueueWorkflow,
 		UpdateFunc: func(old, new interface{}) {
@@ -91,9 +75,6 @@ func NewController(
 	return controller
 }
 
-// enqueueWorkflow takes a Workflow resource and converts it into a namespace/name
-// string which is then put onto the work queue. This method should *not* be
-// passed resources of any type other than Workflow.
 func (c *Controller) enqueueWorkflow(obj interface{}) {
 	var key string
 	var err error
@@ -104,25 +85,18 @@ func (c *Controller) enqueueWorkflow(obj interface{}) {
 	c.workqueue.AddRateLimited(key)
 }
 
-// Run will set up the event handlers for types we are interested in, as well
-// as syncing informer caches and starting workers. It will block until stopCh
-// is closed, at which point it will shutdown the workqueue and wait for
-// workers to finish processing their current work items.
 func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
 	defer runtime.HandleCrash()
 	defer c.workqueue.ShutDown()
 
-	// Start the informer factories to begin populating the informer caches
 	glog.Info("Starting Workflow controller")
 
-	// Wait for the caches to be synced before starting workers
 	glog.Info("Waiting for informer caches to sync")
 	if ok := cache.WaitForCacheSync(stopCh, c.workflowSynced); !ok {
 		return fmt.Errorf("failed to wait for caches to sync")
 	}
 
 	glog.Info("Starting workers")
-	// Launch two workers to process Workflow resources
 	for i := 0; i < threadiness; i++ {
 		go wait.Until(c.runWorker, time.Second, stopCh)
 	}
@@ -134,16 +108,11 @@ func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
 	return nil
 }
 
-// runWorker is a long-running function that will continually call the
-// processNextWorkItem function in order to read and process a message on the
-// workqueue.
 func (c *Controller) runWorker() {
 	for c.processNextWorkItem() {
 	}
 }
 
-// processNextWorkItem will read a single work item off the workqueue and
-// attempt to process it, by calling the syncHandler.
 func (c *Controller) processNextWorkItem() bool {
 	obj, shutdown := c.workqueue.Get()
 
@@ -151,37 +120,18 @@ func (c *Controller) processNextWorkItem() bool {
 		return false
 	}
 
-	// We wrap this block in a func so we can defer c.workqueue.Done.
 	err := func(obj interface{}) error {
-		// We call Done here so the workqueue knows we have finished
-		// processing this item. We also must remember to call Forget if we
-		// do not want this work item being re-queued. For example, we do
-		// not call Forget if a transient error occurs, instead the item is
-		// put back on the workqueue and attempted again after a back-off
-		// period.
 		defer c.workqueue.Done(obj)
 		var key string
 		var ok bool
-		// We expect strings to come off the workqueue. These are of the
-		// form namespace/name. We do this as the delayed nature of the
-		// workqueue means the items in the informer cache may actually be
-		// more up to date that when the item was initially put onto the
-		// workqueue.
 		if key, ok = obj.(string); !ok {
-			// As the item in the workqueue is actually invalid, we call
-			// Forget here else we'd go into a loop of attempting to
-			// process a work item that is invalid.
 			c.workqueue.Forget(obj)
 			runtime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
 			return nil
 		}
-		// Run the syncHandler, passing it the namespace/name string of the
-		// Workflow resource to be synced.
 		if err := c.syncHandler(key); err != nil {
 			return fmt.Errorf("error syncing '%s': %s", key, err.Error())
 		}
-		// Finally, if no error occurs we Forget this item so it does not
-		// get queued again until another change happens.
 		c.workqueue.Forget(obj)
 		glog.Infof("Successfully synced '%s'", key)
 		return nil
@@ -195,22 +145,15 @@ func (c *Controller) processNextWorkItem() bool {
 	return true
 }
 
-// syncHandler compares the actual state with the desired, and attempts to
-// converge the two. It then updates the Status block of the Workflow resource
-// with the current status of the resource.
 func (c *Controller) syncHandler(key string) error {
-	// Convert the namespace/name string into a distinct namespace and name
 	namespace, name, err := cache.SplitMetaNamespaceKey(key)
 	if err != nil {
 		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
 		return nil
 	}
 
-	// Get the Workflow resource with this namespace/name
 	wf, err := c.workflowLister.Workflows(namespace).Get(name)
 	if err != nil {
-		// The Workflow resource may no longer exist, in which case we stop
-		// processing.
 		if errors.IsNotFound(err) {
 			runtime.HandleError(fmt.Errorf("workflow '%s' in work queue no longer exists", key))
 			return nil
@@ -221,14 +164,10 @@ func (c *Controller) syncHandler(key string) error {
 
 	wfName := wf.Spec.Name
 	if wfName == "" {
-		// We choose to absorb the error here as the worker would requeue the
-		// resource otherwise. Instead, the next time the resource is updated
-		// the resource will be queued again.
 		runtime.HandleError(fmt.Errorf("%s: deployment name must be specified", key))
 		return nil
 	}
 
-	// Finally, we update the status block of the Workflow resource to reflect the current state of the world
 	err = c.updateWorkflowStatus(wf)
 	if err != nil {
 		return err
@@ -239,15 +178,8 @@ func (c *Controller) syncHandler(key string) error {
 }
 
 func (c *Controller) updateWorkflowStatus(wf *workflowv1beta.Workflow) error {
-	// NEVER modify objects from the store. It's a read-only, local cache.
-	// You can use DeepCopy() to make a deep copy of original object and modify this copy
-	// Or create a copy manually for better performance
 	wfCopy := wf.DeepCopy()
 	wfCopy.Status.Name = wf.Spec.Name
-	// If the CustomResourceSubresources feature gate is not enabled,
-	// we must use Update instead of UpdateStatus to update the Status block of the Workflow resource.
-	// UpdateStatus will not allow changes to the Spec of the resource,
-	// which is ideal for ensuring nothing other than resource status has been updated.
 	_, err := c.workflowClientset.WorkflowV1beta().Workflows(wf.Namespace).Update(wfCopy)
 	return err
 }
